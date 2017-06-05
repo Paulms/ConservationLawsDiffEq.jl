62b7f16859f2408fb08f361373eb7bfb9db4afb8
Paul Mendez <pmendez@ing-mat.udec.cl>
2017-06-05 19:28:23 -0400

    Update plot recipe
    Fix labels

--- src/plotRecipe.jl
+++ src/plotRecipe.jl
@@ -3,13 +3,18 @@
     xguide --> "x"
     yguide --> "u"
     labels = String[]
-    for i in 1:size(sol.u,2)
+    for i in 1:size(sol.u[tidx],2)
       push!(labels,"u$i")
     end
-    label --> reshape(labels,1,length(labels))
     yvector = sol.u[tidx]
     if uvars != 0
       yvector = sol.u[tidx][:,uvars]
+      labels = labels[uvars]
+    end
+    if typeof(labels) <: String
+      label --> labels
+    else
+      label --> reshape(labels,1,length(labels))
     end
     sol.prob.mesh.x, yvector
 end
