115e90726305b42d4e1c52dc4e2fa9efdecbec02
Paul Mendez <pmendez@ing-mat.udec.cl>
2017-10-17 11:16:58 -0300

    Enhance test suit

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using ConservationLawsDiffEq
 using Base.Test
 
 @time @testset "1D Scalar Algorithms" begin include("test0.jl") end
-@time @testset "1D Sytems Algorithms" begin include("test2.jl") end
-@time @testset "1D Diffusion System Algorithms" begin include("test4.jl") end
+#@time @testset "1D Sytems Algorithms" begin include("test2.jl") end
+#@time @testset "1D Diffusion System Algorithms" begin include("test4.jl") end
--- test/test0.jl
+++ test/test0.jl
@@ -2,9 +2,18 @@
 # u_t+(0.5*u²)_{x}=0
 
 using ConservationLawsDiffEq
+include("burgers.jl")
 
 const CFL = 0.5
 const Tend = 1.0
+const ul = 0.0
+const ur = 1.0
+const x0 = 0.0
+const xl = -3.0
+const xr = 3.0
+
+prob1 = RiemannProblem(Burgers(), ul, ur, x0, 0.0)
+sol_ana  = solve(prob1)
 
 f(::Type{Val{:jac}},u::Vector) = diagm(u)
 f(u::Vector) = u.^2/2
@@ -12,19 +21,41 @@ f(u::Vector) = u.^2/2
 function u0_func(xx)
   N = size(xx,1)
   uinit = zeros(N, 1)
-  uinit[:,1] = sin.(2*π*xx)
+  for (i,x) in enumerate(xx)
+      uinit[i,1] = (x < x0) ? ul : ur
+  end
   return uinit
 end
 
 function get_problem(N)
-  mesh = Uniform1DFVMesh(N,0.0,1.0,:PERIODIC)
-  u0 = u0_func(mesh.x)
+  mesh = Uniform1DFVMesh(N,xl,xr,:PERIODIC, :PERIODIC)
+  u0 = u0_func(cell_centers(mesh))
   ConservationLawsProblem(u0,f,CFL,Tend,mesh)
 end
-prob = get_problem(10)
-@time sol = solve(prob, FVSKTAlgorithm();progress=true)
-@time sol2 = solve(prob, LaxFriedrichsAlgorithm();progress=true)
-#@time sol3 = solve(prob, LaxWendroff2sAlgorithm();progress=true)
-@time sol4 = solve(prob, FVCompWENOAlgorithm();progress=true, TimeAlgorithm = SSPRK33())
-@time sol5 = solve(prob, FVCompMWENOAlgorithm();progress=true, TimeAlgorithm = SSPRK33())
-true
+prob = get_problem(50)
+@time sol = solve(prob, FVSKTAlgorithm();progress=true, use_threads = true, save_everystep = false)
+@time sol1 = fast_solve(prob, FVSKTAlgorithm();progress=true)
+@time sol2 = solve(prob, LaxFriedrichsAlgorithm();progress=true, save_everystep = false)
+@time sol3 = solve(prob, LocalLaxFriedrichsAlgorithm();progress=true, save_everystep = false)
+@time sol4 = solve(prob, GlobalLaxFriedrichsAlgorithm();progress=true, save_everystep = false)
+#@time sol5 = solve(prob, LaxWendroff2sAlgorithm();progress=true, save_everystep = false)
+@time sol5 = solve(prob, FVCompWENOAlgorithm();progress=true, TimeAlgorithm = SSPRK33())
+@time sol6 = solve(prob, FVCompMWENOAlgorithm();progress=true, TimeAlgorithm = SSPRK33())
+
+#Compute approximate errors at tend
+function calc_error(uana, unum::AbstractFVSolution, tend, xl, xr)
+    N = numcells(unum.prob.mesh)
+    xk = cell_centers(unum.prob.mesh)
+    uexact = zeros(N)
+    for (i,x) = enumerate(xk)
+        uexact[i] = uana(tend, x)
+    end
+    sum(1.0/N*abs.(unum.u[end] - uexact))
+end
+@test calc_error(sol_ana, sol, Tend, -2.0, 2.0) < 0.095
+@test calc_error(sol_ana, sol1, Tend, -2.0, 2.0) < 0.095
+@test calc_error(sol_ana, sol2, Tend, -2.0, 2.0) < 0.14
+@test calc_error(sol_ana, sol3, Tend, -2.0, 2.0) < 0.13
+@test calc_error(sol_ana, sol4, Tend, -2.0, 2.0) < 0.14
+@test calc_error(sol_ana, sol5, Tend, -2.0, 2.0) < 0.095
+@test calc_error(sol_ana, sol6, Tend, -2.0, 2.0) < 0.095
